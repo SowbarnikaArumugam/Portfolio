fbf066cf6ebba3843b51696e69a6f6e06b2ac5f4
SowbarnikaArumugam <sowmipavi02@gmail.com>
2024-01-25 11:26:49 +0530

    php added

---
+++ process_form.php
@@ -0,0 +1,20 @@
+<?php
+ if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $number = $_POST["number"];
+    $msg = $_POST["msg"];
+
+    $to = "sowmipavi02@gmail.com";
+    $subject = "portfolio";
+    $message = "Name: $name\n";
+    $message .= "Email: $email\n";
+    $message .= "Phone number: $number\n";
+    $message .= "Message: $msg\n";
+    
+    mail($to, $subject, $message);
+
+    echo "<p>Messsage is sent successfully!!</p>";
+ }
+
+ ?>
